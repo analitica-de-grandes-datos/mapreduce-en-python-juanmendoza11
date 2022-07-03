6e5493160bb3eb23688dd1650876cb99a1e42be5
Juan Sebastian Mendoza <jmendozaco@unal.edu.co>
2022-07-02 23:34:29 -0500

    -

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+# Clave - valor
+
+import sys
+if __name__ == "__main__":
+  for line in sys.stdin:
+    col = line.split(',')
+    col_credit_history = col[2]
+    sys.stdout.write("{}\t1\n".format(col_credit_history))
