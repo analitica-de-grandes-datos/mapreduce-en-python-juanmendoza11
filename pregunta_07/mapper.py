d13053609212e4341fdff5ddbdb9d5eb077205aa
Juan Sebastian Mendoza <jmendozaco@unal.edu.co>
2022-07-02 23:52:09 -0500

    -

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,10 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-
+import sys 
 if __name__ == "__main__":
-    for line in sys.stdin:
-        tercera_columna = line.split('   ')[2].strip()
-        tercera_columna = tercera_columna.zfill(4)
-        sys.stdout.write("{} {} {}\n".format(
-            str(line.split('   ')[0]), tercera_columna, line.split('   ')[1]))
+ for line in sys.stdin:
+           letter = line.split('   ')[0]
+           date = line.split('   ')[1]
+           number = int(line.split('   ')[2])
+           sys.stdout.write("{}\t{}\t{}\n".format(letter,date,number))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,10 +1,13 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
-
 import sys
-if __name__ == "__main__":
-    for line in sys.stdin:
-        lista = line.split(' ')
-        sys.stdout.write("{}   {}   {}\n".format(
-            lista[0], lista[2].strip(), int(lista[1])))
+if __name__ == '__main__':
+ campo_lista=[]
+ for line in sys.stdin:
+    marca, date, value = line.split("\t") 
+    value=int(value) 
+    campo_lista.append([marca, date, value]) 
+ campo_lista = sorted(campo_lista, key=lambda x: (x[0], x[2])) 
+ for line in campo_lista: 
+    sys.stdout.write(line[0] +"   " + line[1] +"   " + str(line[2]) + "\n")
